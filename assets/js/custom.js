727851921a4d4ee539cb6296d8b27f4742b977d7
unknown <tshafique51@gmail.com>
2023-04-16 02:02:12 -0700

    some changes to navbaar and login forms

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -16,3 +16,91 @@ const closeNavMenu = () => {
     smNav.style.display = 'none'
 }
 navClose.addEventListener("click", closeNavMenu)
+
+
+
+// slider for mobile view
+
+$(document).ready(function(){
+    $('.slider-mobile').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        // dots: true,
+        arrows : true
+    });
+  });
+
+// login form 
+
+function formController(){
+
+
+
+
+// nav menu login button 
+var loginButton = document.querySelector(".nav-login");
+var loginForm = document.querySelector(".log-in-form");
+
+// displays main login form
+loginButton.addEventListener('click', function(){
+    loginForm.style.display = 'flex' , 'important';
+});
+
+// close login form
+var loginFormClose = document.querySelector(".login-form-close");
+loginFormClose.addEventListener('click', function(event){
+    event.preventDefault()
+    loginForm.style.display = 'none' , 'important'
+})
+
+
+
+
+
+// sign up form
+var signUpForm = document.querySelector(".sign-up-form");
+var regiserBtn = document.querySelector(".register");
+regiserBtn.addEventListener('click', function(event){
+    event.preventDefault();
+    signUpForm.style.display = 'flex', 'important'
+    loginForm.style.display = 'none'
+});
+
+// sign up form close
+var signupFormClose = document.querySelector(".signup-form-close");
+signupFormClose.addEventListener('click', function(event){
+    event.preventDefault()
+    signUpForm.style.display = 'none' , 'important'
+});
+
+// forget password form display
+var passwordResetForm = document.querySelector(".forget-password-form");
+var resetBtn = document.querySelector(".forget");
+resetBtn.addEventListener('click', function(event){
+    event.preventDefault();
+    passwordResetForm.style.display = 'flex', 'important'
+    loginForm.style.display ='none'
+    signUpForm.style.display ='none'
+
+});
+
+// forget password form close
+var resetFormClose = document.querySelector(".reset-form-close");
+resetFormClose.addEventListener('click', function(){
+    // event.preventDefault()
+    passwordResetForm.style.display = 'none' 
+});
+
+// register form to sign in form 
+var signUpIn = document.querySelector(".sign-in-2");
+signUpIn.addEventListener('click', function(event){
+    event.preventDefault();
+    loginForm.style.display = 'flex';
+    signUpForm.style.display = 'none';
+})
+
+}
+formController();
+
+
+
